ba977011090efc095b99c398ff236110c8412761
mkmu260 <68840505+mkmu260@users.noreply.github.com>
2021-03-08 15:09:03 +1300

    Fix #1

--- calculator.py
+++ calculator.py
@@ -1,30 +1,34 @@
 print("Welcome to Manoj's Calculator V69")
 exitProgram = False
 while(exitProgram == False):
-    try:
-        firstNum = int(input("Enter your first number (If you want to exit, enter exit): "))
-    except ValueError:
+    firstNum = input("Enter your first number (If you want to exit, enter exit): ")
+    if(firstNum == "exit"):
         print("Thank you for using Manoj K Services inc.")
         exitProgram = True
     else:
-        calcType = input("What calculation do you want to do? (enter the symbol): ")
         try:
-            secondNum = int(input("Enter your second number: "))
-        except:
+            firstNum = int(firstNum)
+        except ValueError:
             print("That is not a valid number")
         else:
-
-            if calcType == '+':
-                output = firstNum + secondNum
-            elif calcType == '-':
-                output = firstNum - secondNum
-            elif calcType == 'x':
-                output = firstNum * secondNum
-            elif calcType == '*':
-                output = firstNum * secondNum
-            elif calcType == '/':
-                output = firstNum / secondNum
+            calcType = input("What calculation do you want to do? (enter the symbol): ")
             try:
-                print("The answer is:", output)
-            except: 
-                print("Your calculation type is invalid, please try again")
+                secondNum = int(input("Enter your second number: "))
+            except ValueError:
+                print("That is not a valid number")
+            else:
+
+                if calcType == '+':
+                    output = firstNum + secondNum
+                elif calcType == '-':
+                    output = firstNum - secondNum
+                elif calcType == 'x':
+                    output = firstNum * secondNum
+                elif calcType == '*':
+                    output = firstNum * secondNum
+                elif calcType == '/':
+                    output = firstNum / secondNum
+                try:
+                    print("The answer is:", output)
+                except: 
+                    print("Your calculation type is invalid, please try again")
